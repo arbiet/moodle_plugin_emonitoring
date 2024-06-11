386dc399345cbe24f673adbf8698e800a1dde3e1
Firmansyah Mukti Wijaya <iki.mukti@gmail.com>
2024-06-11 07:07:00 +0000

    Initial commit of Moodle files

---
+++ blocks/completion_progress/lang/id/block_completion_progress.php
@@ -0,0 +1,108 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * E-Monitoring block Indonesian language translation
+ *
+ * @package    block_completion_progress
+ * @copyright  2024 Amalia Puspitasari
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['completed_colour'] = '#73A839';
+$string['completed_colour_descr'] = 'Kode warna HTML untuk elemen yang telah diselesaikan';
+$string['completed_colour_title'] = 'Warna selesai';
+$string['completion_not_enabled'] = 'Pelacakan penyelesaian tidak diaktifkan di situs ini.';
+$string['completion_not_enabled_course'] = 'Pelacakan penyelesaian tidak diaktifkan di kursus ini.';
+$string['completion_progress:addinstance'] = 'Tambahkan blok E-Monitoring baru';
+$string['completion_progress:myaddinstance'] = 'Tambahkan blok E-Monitoring ke halaman Beranda saya';
+$string['completion_progress:overview'] = 'Lihat ikhtisar kursus E-Monitoring untuk semua siswa';
+$string['completion_progress:showbar'] = 'Tampilkan bilah di blok E-Monitoring';
+$string['config_activitiesincluded'] = 'Aktivitas yang disertakan';
+$string['config_activitycompletion'] = 'Semua aktivitas dengan penyelesaian diatur';
+$string['config_default_title'] = 'E-Monitoring';
+$string['config_group'] = 'Hanya terlihat untuk grup atau pengelompokan';
+$string['config_header_monitored'] = 'Dimonitor';
+$string['config_icons'] = 'Gunakan ikon di bilah';
+$string['config_longbars'] = 'Bagaimana menyajikan bilah panjang';
+$string['config_orderby'] = 'Urutkan bilah berdasarkan';
+$string['config_orderby_course_order'] = 'Pengurutan dalam kursus';
+$string['config_orderby_due_time'] = 'Waktu menggunakan tanggal "{$a}"';
+$string['config_percentage'] = 'Tampilkan persentase kepada siswa';
+$string['config_scroll'] = 'Gulir';
+$string['config_selectactivities'] = 'Pilih aktivitas';
+$string['config_selectedactivities'] = 'Aktivitas terpilih';
+$string['config_squeeze'] = 'Peras';
+$string['config_title'] = 'Judul alternatif';
+$string['config_wrap'] = 'Bungkus';
+$string['coursenametoshow'] = 'Nama kursus untuk ditampilkan di Dasbor';
+$string['defaultlongbars'] = 'Presentasi default untuk bilah panjang';
+$string['forceiconsinbar'] = 'Paksa ikon di bilah kemajuan';
+$string['fullname'] = 'Nama kursus lengkap';
+$string['futureNotCompleted_colour'] = '#025187';
+$string['futureNotCompleted_colour_descr'] = 'Kode warna HTML untuk elemen mendatang yang belum selesai';
+$string['futureNotCompleted_colour_title'] = 'Warna belum selesai di masa mendatang';
+$string['how_activitiesincluded_works'] = 'Bagaimana cara menyertakan aktivitas';
+$string['how_activitiesincluded_works_help'] = '<p>Secara default, semua aktivitas dengan pengaturan penyelesaian aktivitas disertakan dalam bilah.</p><p>Anda juga dapat memilih aktivitas untuk disertakan secara manual.</p>';
+$string['how_group_works'] = 'Cara kerja grup yang terlihat';
+$string['how_group_works_help'] = '<p>Memilih grup atau pengelompokan akan membatasi tampilan blok ini hanya untuk grup atau pengelompokan tersebut.</p>';
+$string['how_longbars_works'] = 'Bagaimana bilah panjang disajikan';
+$string['how_longbars_works_help'] = '<p>Ketika bilah melebihi panjang yang ditetapkan, bagaimana mereka dapat disajikan dalam salah satu cara berikut.</p><ul><li>Ditekan menjadi satu bilah horizontal</li><li>Gulir ke samping untuk menunjukkan segmen bilah yang meluap</li><li>Membungkus untuk menunjukkan semua segmen bilah dalam beberapa baris</li></ul><p>Perhatikan bahwa saat bilah dibungkus, indikator SEKARANG tidak akan ditampilkan.</p>';
+$string['how_ordering_works'] = 'Bagaimana pengurutan bekerja';
+$string['how_ordering_works_help'] = '<p>Ada dua cara untuk mengurutkan aktivitas di blok E-Monitoring.</p><ul><li><em>Waktu menggunakan tanggal "Harapkan selesai pada"</em> (default)<br />Tanggal penyelesaian yang diharapkan dari aktivitas/sumber daya digunakan untuk mengurutkan bilah. Jika aktivitas/sumber daya tidak memiliki tanggal penyelesaian yang diharapkan, pengurutan dalam kursus digunakan sebagai gantinya. Saat opsi ini digunakan, indikator SEKARANG ditampilkan.</li><li><em>Pengurutan dalam kursus</em><br />Aktivitas/sumber daya disajikan dalam urutan yang sama seperti yang ada di halaman kursus. Saat opsi ini digunakan, tanggal penyelesaian yang diharapkan diabaikan. Saat opsi ini digunakan, indikator SEKARANG tidak ditampilkan.</li></ul>';
+$string['how_selectactivities_works'] = 'Bagaimana cara menyertakan aktivitas';
+$string['how_selectactivities_works_help'] = '<p>Untuk memilih aktivitas yang akan disertakan dalam bilah secara manual, pastikan bahwa "Aktivitas yang disertakan" diatur ke "aktivitas terpilih".</p><p>Hanya aktivitas dengan pengaturan penyelesaian aktivitas yang dapat disertakan.</p><p>Tahan tombol CTRL untuk memilih beberapa aktivitas.</p>';
+$string['indeterminate'] = '?';
+$string['lastonline'] = 'Terakhir di kursus';
+$string['mouse_over_prompt'] = 'Arahkan mouse atau sentuh bilah untuk info.';
+$string['no_activities_config_message'] = 'Tidak ada aktivitas atau sumber daya yang diatur penyelesaiannya atau tidak ada aktivitas atau sumber daya yang dipilih. Atur penyelesaian aktivitas pada aktivitas dan sumber daya, kemudian konfigurasikan blok ini.';
+$string['no_activities_message'] = 'Tidak ada aktivitas atau sumber daya yang dimonitor. Gunakan konfigurasi untuk mengatur pemantauan.';
+$string['no_blocks'] = 'Tidak ada blok E-Monitoring yang diatur untuk kursus Anda.';
+$string['no_courses'] = "Anda tidak terdaftar di kursus apa pun. Hanya bilah dari kursus yang terdaftar yang akan ditampilkan.";
+$string['no_visible_activities_message'] = 'Tidak ada aktivitas yang dimonitor yang saat ini terlihat.';
+$string['not_all_expected_set'] = 'Tidak semua aktivitas dengan penyelesaian memiliki tanggal "{$a}" yang diatur.';
+$string['notCompleted_colour'] = '#C71C22';
+$string['notCompleted_colour_descr'] = 'Kode warna HTML untuk elemen saat ini yang belum selesai';
+$string['notCompleted_colour_title'] = 'Warna belum selesai';
+$string['now_indicator'] = 'SEKARANG';
+$string['overview'] = 'Ikhtisar siswa';
+$string['pluginname'] = 'E-Monitoring';
+$string['progress'] = 'Kemajuan';
+$string['progressbar'] = 'E-Monitoring';
+$string['privacy:metadata'] = 'Blok E-Monitoring hanya menampilkan data penyelesaian yang ada.';
+$string['selectitem'] = 'Pilih \'{$a}\'';
+$string['shortname'] = 'Nama kursus singkat';
+$string['showallinfo'] = 'Tampilkan semua info';
+$string['showinactive'] = 'Tampilkan siswa tidak aktif di Ikhtisar';
+$string['showlastincourse'] = 'Tampilkan waktu terakhir siswa di kursus di Ikhtisar';
+$string['submitted'] = 'Dikirim';
+$string['submittednotcomplete_colour'] = '#FFCC00';
+$string['submittednotcomplete_colour_descr'] = 'Kode warna HTML untuk elemen yang telah dikirimkan, tetapi belum selesai';
+$string['submittednotcomplete_colour_title'] = 'Warna dikirim tetapi belum selesai';
+$string['time_expected'] = 'Diharapkan';
+$string['why_set_the_title'] = 'Mengapa Anda mungkin ingin mengatur judul instance blok?';
+$string['why_set_the_title_help'] = '<p>Blok E-Monitoring dapat memiliki beberapa instance. Anda dapat menggunakan blok E-Monitoring yang berbeda untuk memantau set aktivitas atau sumber daya yang berbeda. Misalnya, Anda dapat melacak kemajuan dalam tugas di satu blok dan kuis di blok lain. Untuk alasan ini, Anda dapat mengganti judul default dan mengatur judul blok yang lebih sesuai untuk setiap instance.</p>';
+$string['why_show_precentage'] = 'Mengapa menunjukkan persentase kemajuan kepada siswa?';
+$string['why_show_precentage_help'] = '<p>Dimungkinkan untuk menunjukkan persentase kemajuan keseluruhan kepada siswa.</p><p>Ini dihitung sebagai jumlah aktivitas yang diselesaikan dibagi dengan jumlah total aktivitas di bilah.</p><p>Persentase kemajuan muncul hingga siswa mengarahkan mouse ke item di bilah.</p>';
+$string['why_use_icons'] = 'Mengapa Anda mungkin ingin menggunakan ikon?';
+$string['why_use_icons_help'] = '<p>Anda mungkin ingin menambahkan ikon centang dan silang di E-Monitoring untuk membuat blok ini lebih mudah diakses secara visual bagi siswa dengan kebutaan warna.</p><p>Ini juga dapat membuat makna blok lebih jelas jika Anda percaya warna tidak intuitif, baik karena alasan budaya atau pribadi.</p>';
+$string['wrapafter'] = 'Saat membungkus, batasi baris hingga';
+$string['not_completed'] = 'Tidak selesai';
+$string['completed'] = 'Selesai';
+$string['kelompok'] = 'Kelompok Siswa';
+$string['nilai'] = 'Nilai';
+$string['grade_col'] = 'Grade Col';
+$string['gradeitem'] = 'Grade Items';
